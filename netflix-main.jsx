805dd7e48de5b298bb8bd72c4e49735bbb137ee7
vaishnav jadhav <142258029+Vaishnav-Jadhav@users.noreply.github.com>
2024-08-21 12:43:05 +0530

    Add files via upload

---
+++ netflix-main.jsx
@@ -0,0 +1,11 @@
+import '../netflix-main/netflix-main.css'
+
+export function NetflixMain()
+{
+    return(
+        <main>
+            <p className="main-title">Unlimited Movies, TV shows and more</p>
+            <p className="main-subtitle">Watch anywhere. Cancel anytime.</p>
+        </main>
+    );
+}
